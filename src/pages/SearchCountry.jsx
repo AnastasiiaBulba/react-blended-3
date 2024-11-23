d2e85890bb1d20e2698cbf319adb661b3b25ea21
Artur Luniaka <artur01049718@gmail.com>
2024-11-23 11:13:08 +0100

    added searchCountries

--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,49 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryList,
+  Heading,
+  Loader,
+  SearchForm,
+  Section,
+} from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/countryApi';
 
 const SearchCountry = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const region = searchParams.get('region');
+    if (!region) return;
+    const fetchCountries = async () => {
+      try {
+        setIsLoading(true);
+        const countries = await fetchByRegion(region);
+        setCountries(countries);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCountries();
+  }, [searchParams]);
+
+  const onSubmit = region => {
+    setSearchParams({ region });
+  };
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm onSubmit={onSubmit} />
+        <CountryList countries={countries} />
+        {error && <Heading title={error} bottom />}
+        {isLoading && <Loader />}
       </Container>
     </Section>
   );
